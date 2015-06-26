5ef75890976df43c1d8088e89e660280925b5aea
leohahn <lhahn@inf.ufrgs.br>
2015-06-26 16:33:44 -0300

    instancias adicionadas

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+class Vector {
+public:
+    Vector(int size): sz{size}, elem{new int[size]} {}
+private:
+    int sz;
+    int* elem;
+};
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
