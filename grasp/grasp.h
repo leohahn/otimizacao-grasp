14963675ea9b3b82ccaec8896a86ed0318cf3ded
leohahn <lhahn@inf.ufrgs.br>
2015-06-27 20:55:34 -0300

    Classe Abstrata GRASP

---
+++ grasp/grasp.h
@@ -0,0 +1,17 @@
+#ifndef __GRASP_H__
+
+//!
+//! Classe abstrata GRASP, que contém os métodos necessários para a implementação
+//! do algoritmo. A classe é extendida de acordo com o problema escolhido
+//!
+class GRASP {
+public:
+    virtual void run() = 0;
+    virtual void constructGreddyRandomSolution() = 0;
+    virtual void makeLocalSearch() = 0;
+    virtual void updateSolution() = 0;
+
+    virtual ~GRASP() {}
+};
+
+#endif
