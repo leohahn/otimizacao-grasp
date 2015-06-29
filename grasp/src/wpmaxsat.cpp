fc468a367c80872295ee39d2a7644fa307741909
leohahn <lhahn@inf.ufrgs.br>
2015-06-29 02:10:53 -0300

    m

--- grasp/src/wpmaxsat.cpp
+++ grasp/src/wpmaxsat.cpp
@@ -259,11 +259,9 @@ vector<bool> WpMaxSAT::makeLocalSearch(vector<bool> solution)
                 all_zeros = false;
             }
         }
-        cout << "ALLZEROS: " << all_zeros << endl;
         if (all_zeros == false) {
             do {
                 val_index = (rand() % (hard_decreasing_vars.size() - 1)) + 1;
-                cout << "index: " << val_index <<endl;
             } while (hard_decreasing_vars[val_index] == 0);
             value = hard_decreasing_vars[val_index];
 
@@ -276,26 +274,29 @@ vector<bool> WpMaxSAT::makeLocalSearch(vector<bool> solution)
             // TODO: Select the best (not random)
             all_zeros = true;
             for (unsigned int i=1; i<soft_decreasing_vars.size(); ++i) {
-                if (soft_decreasing_vars[i] > 1) {
+                if (soft_decreasing_vars[i] > 0) {
                     all_zeros = false;
                 }
             }
-            cout << "HARD DECREASING VARS: " <<endl;
 
             if (all_zeros == false) {
                 do {
                     val_index = (rand() % (soft_decreasing_vars.size() - 1)) + 1;
-                    cout << "IMPRIMIR UMA VEZ AHAHAHHAHAHHAHAHHAHAHAHAH" << endl;
-
-                    printIntVector(hard_decreasing_vars);
                 } while (hard_decreasing_vars[val_index] > 0);
-                cout << "AVABOU O WHILE" << endl;
+
+                int best_index = 1;
+                for (int i=2; i<soft_decreasing_vars.size(); ++i) {
+                    if (soft_decreasing_vars[i] > soft_decreasing_vars[best_index]) {
+                        best_index = i;
+                    }
+                }
+                printIntVector(soft_decreasing_vars);
                 value = soft_decreasing_vars[val_index];
 
-                if (current_sol[val_index] == true) {
-                    current_sol[val_index] = false;
+                if (current_sol[best_index] == true) {
+                    current_sol[best_index] = false;
                 } else {
-                    current_sol[val_index] = true;
+                    current_sol[best_index] = true;
                 }
             }
         }
